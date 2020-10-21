3183a06c3b18903764f6cf9dbd772362790e647a
kaji <kaji.vertex@gmail.com>
2020-10-21 13:35:26 +0800

    2nd new update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,7 +11,7 @@ export class HomePage {
     {
       "id": 1,
       "title": "Tennis Rules!",
-      "desc": "Tennis is a Tennis is a sport that originated in England around the 19th century and is now played in a host of countries around the world. There are four major tournaments known as the [Majors] that include Wimbledon, US Open, French Open and Australian tournament.",
+      "desc": "Tennis is a sport that originated in England around the 19th century and is now played in a host of countries around the world. There are four major tournaments known as the [Majors] that include Wimbledon, US Open, French Open and Australian tournament.",
       "owner": "Raj Tatavarthy",
     }
   ];
